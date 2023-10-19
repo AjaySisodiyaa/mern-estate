aed926c77610b7d2fabe61b6c9326027828809d1
AjaySisodiyaa <ajay24sisodiya@gmail.com>
2023-10-19 14:35:55 +0530

    create signup api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,13 +1,15 @@
 import User from "../models/user.model.js";
-import mongoose from "mongoose";
+import bcryptjs from "bcryptjs";
 
 export const signup = async (req, res) => {
-  // await mongoose
-  //   .connect(process.env.MONGO)
-  //   .then(() => console.log("Connected with db"));
   const { username, email, password } = req.body;
-  const newUser = new User({ username, email, password });
-  await newUser.save();
-  console.log(newUser);
-  res.status(201).json("User Created Successfully");
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    console.log(newUser);
+    res.status(201).json("User Created Successfully");
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
 };
